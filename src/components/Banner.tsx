c48276740b9f1a8e7e7578a3908abcdc774933d1
EternalWill43 <emilsmoney@live.com>
2022-04-15 08:05:28 -0400

    Project skeleton

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+
+
+function Banner() {
+    return (
+        <div className="banner">
+            <h1>
+                Memory Game
+            </h1>
+        </div>
+    )
+}
+
+export default Banner;
