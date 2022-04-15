d0d6792e4e7912d61cfe1cc4d7204cf38bde6bfa
EternalWill43 <emilsmoney@live.com>
2022-04-15 09:53:26 -0400

    Added functioning score

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css';
 import Pokemon from './components/Pokemon';
 import Banner from './components/Banner';
+import { useState, useEffect, SetStateAction } from 'react';
+
+export interface cProps {
+  score: number;
+  setScore: React.Dispatch<SetStateAction<number>>;
+  guesses: string[];
+  setGuesses: React.Dispatch<SetStateAction<string[]>>;
+}
 
 function App() {
-  //const [score, setScore] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [guesses, setGuesses] = useState<string[]>([]);
+  useEffect(() => {
+    setScore(0);
+  }, [])
   return (
     <div className="App">
       <Banner/>
-      <Pokemon/>
+      <div>Your score is: {score}</div>
+      <Pokemon guesses={guesses} score={score} setScore={setScore} setGuesses={setGuesses}/>
     </div>
   );
 }
