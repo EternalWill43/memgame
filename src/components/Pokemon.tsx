dcba657ee6e8df89943fd6aa4710dc2f67d52c27
EternalWill43 <emilsmoney@live.com>
2022-04-15 09:00:03 -0400

    fixed card layout

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -2,22 +2,27 @@ import { Card } from '@mui/material';
 import {useEffect, useState} from 'react';
 
 function Pokemon() {
-    const [url, setUrl] = useState('');
-    const [name, setName] = useState('');
+    const [url, setUrl] = useState<string[]>([]);
+    const [name, setName] = useState<string[]>([]);
     useEffect(() => {
-        let num = Math.floor(Math.random() * 150) + 1;
-        fetch('https://pokeapi.co/api/v2/pokemon/' + num)
-        .then(response => {
-            if (response.ok) return response.json();
-        })
-        .then(data => { 
-            console.log(data);
-            setName(data.species.name);
-            setUrl(data.sprites['front_default']);
-        })
+        for (let i = 0; i < 10; i++) 
+        {
+            let num = Math.floor(Math.random() * 150) + 1;
+            fetch('https://pokeapi.co/api/v2/pokemon/' + num)
+            .then(response => {
+                if (response.ok) return response.json();
+            })
+            .then(data => { 
+                console.log(data);
+                setName(prev => [...prev, data.species.name]);
+                setUrl(prev => [...prev, data.sprites['front_default']]);
+            })
+        }
     }, []);
     return (
-        <div>{url ? <Card className="pokecard" sx={{m: 5, maxWidth: 150}}><img src={url}/><span>{name}</span></Card> : <div>Loading</div>}</div>
+        <div className="container">{url.map((lnk, i) => 
+            <Card sx={{m: 5, maxWidth:150}} className="pokecard"><img src={lnk}/><span className="name">{name[i]}</span></Card>)}     
+        </div>
     )
 }
 
